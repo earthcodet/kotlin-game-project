d29896b569406e11161622c30aa20121f1dd6b81
heroserth2 <59160969@go.buu.ac.th>
2019-11-07 12:08:37 +0700

    create Room Database

---
+++ app/src/main/java/com/s59160969/least/database/LeastDatabase.kt
@@ -0,0 +1,34 @@
+package com.s59160969.least.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [LeastScore::class],version = 1,exportSchema = false)
+abstract class LeastDatabase : RoomDatabase() {
+    abstract val leastDatabaseDAO: LeastDatabaseDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: LeastDatabase? = null
+
+        fun geInstance(context: Context):LeastDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        LeastDatabase::class.java,
+                        "data_least_table"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
