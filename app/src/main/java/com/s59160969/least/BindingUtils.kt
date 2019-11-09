3f6f4931d8c6b134a07dfab56a0a734879930724
heroserth2 <59160969@go.buu.ac.th>
2019-11-09 15:59:37 +0700

    add top color scoreBoard

--- app/src/main/java/com/s59160969/least/BindingUtils.kt
+++ app/src/main/java/com/s59160969/least/BindingUtils.kt
@@ -1,5 +1,7 @@
 package com.s59160969.least
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -22,11 +24,21 @@ fun TextView.setRankingScoreString(item: LeastScore) {
     text = getIndexScoreboard().toString()
 }
 
-@BindingAdapter("scoreImage")
-fun ImageView.setScoreImage(item: LeastScore) {
+@BindingAdapter("firstScoreImage")
+fun ImageView.setFirstScoreImage(item: LeastScore) {
    when (getIndexScoreboard()) {
         1 -> setImageResource(R.drawable.medal)
         else -> visibility = View.GONE
+    }
+}
+@SuppressLint("ResourceAsColor")
+@BindingAdapter("topBackgroundScore")
+fun View.setTopBackgroundScore(item: LeastScore) {
+    when (getIndexScoreboard()) {
+        1 ->  setBackgroundColor(Color.parseColor("#e3b10b"))
+        2 ->  setBackgroundColor(Color.parseColor("#cacdd4"))
+        3 ->  setBackgroundColor(Color.parseColor("#d6802e"))
+        else -> setBackgroundColor(Color.parseColor("#FFFFFF"))
     }
 }
 
